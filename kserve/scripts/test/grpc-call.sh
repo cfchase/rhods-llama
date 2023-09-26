04d94d123ee7c039d02bbb60bf2d301b1f7b06b4
Chris Chase <cchase@redhat.com>
2023-09-26 00:02:21 -0400

    Use public s3 bucket

--- kserve/scripts/test/grpc-call.sh
+++ kserve/scripts/test/grpc-call.sh
@@ -13,15 +13,15 @@ echo "Wait until runtime is READY"
 wait_for_pods_ready "serving.kserve.io/inferenceservice=caikit-example-isvc" "${TEST_NS}"
 oc wait --for=condition=ready pod -l serving.kserve.io/inferenceservice=caikit-example-isvc -n ${TEST_NS} --timeout=300s
 
+export KSVC_HOSTNAME=$(oc get ksvc caikit-example-isvc-predictor -n ${TEST_NS} -o jsonpath='{.status.url}' | cut -d'/' -f3)
+echo $KSVC_HOSTNAME
+
 echo
 echo "Testing all token in a single call"
 echo
-
-export KSVC_HOSTNAME=$(oc get ksvc caikit-example-isvc-predictor -n ${TEST_NS} -o jsonpath='{.status.url}' | cut -d'/' -f3)
 grpcurl -insecure -d '{"text": "At what temperature does liquid Nitrogen boil?"}' -H "mm-model-id: flan-t5-small-caikit" ${KSVC_HOSTNAME}:443 caikit.runtime.Nlp.NlpService/TextGenerationTaskPredict
 
 echo
 echo "Testing streams of token"
 echo
-
 grpcurl -insecure -d '{"text": "At what temperature does liquid Nitrogen boil?"}' -H "mm-model-id: flan-t5-small-caikit" ${KSVC_HOSTNAME}:443 caikit.runtime.Nlp.NlpService/ServerStreamingTextGenerationTaskPredict
